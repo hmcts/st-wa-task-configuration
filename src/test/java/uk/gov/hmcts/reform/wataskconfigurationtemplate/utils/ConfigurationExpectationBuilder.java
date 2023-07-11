21ddd2a3e9fb82fffc6c5f362603f0af602b0a1c
abhiahirv1 <abhi.ahir@hmcts.net>
2023-07-11 16:08:08 +0100

    Unit test for Configuration test

---
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/utils/ConfigurationExpectationBuilder.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.wataskconfigurationtemplate.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConfigurationExpectationBuilder {
+    private static List<String> EXPECTED_PROPERTIES = Arrays.asList(
+      "majorPriority","minorPriority","workType", "roleCategory","dueDateIntervalDays"
+    );
+    private static DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+
+    private Map<String,Map<String,Object>> expectations = new HashMap<>();
+
+    public static ConfigurationExpectationBuilder defaultExpectations() {
+        ConfigurationExpectationBuilder builder = new ConfigurationExpectationBuilder();
+        builder.expectedValue("workType", "routine_work", true);
+        builder.expectedValue("roleCategory", "ADMIN", true);
+        builder.expectedValue("minorPriority", "500", true);
+        builder.expectedValue("majorPriority", "5000", true);
+        builder.expectedValue("dueDateIntervalDays", "5", true);
+        return builder;
+    }
+
+    public List<Map<String,Object>> build() {
+        return EXPECTED_PROPERTIES.stream()
+            .filter(p -> expectations.containsKey(p))
+            .map(p -> expectations.get(p))
+            .collect(Collectors.toList());
+    }
+
+    public ConfigurationExpectationBuilder expectedValue(String name, Object value, boolean canReconfigure) {
+        expectations.put(name, Map.of(
+            "name", name,
+            "value", value,
+            "canReconfigure", canReconfigure
+        ));
+        return this;
+    }
+
+    public static String now() {
+        return LocalDateTime.now().format(dateTimeFormat);
+    }
+}
