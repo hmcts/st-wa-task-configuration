bc0552ecb3f2ef743d2fa3f1a41f9f0a5d9c21ea
abhiahirv1 <abhi.ahir@hmcts.net>
2023-07-14 14:45:10 +0100

    Update DMN files (initiation, permissions, configuration) and Unit test for WA - Admin Task - 'Process Written Reasons'

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
@@ -76,6 +76,10 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
             Map.of(
                 "taskTypeId", "processOtherDirectionsReturned",
                 "taskTypeName", "Process other directions reurned"
+            ),
+            Map.of(
+                "taskTypeId", "processWrittenReasons",
+                "taskTypeName", "Process Written Reasons"
             )
         );
         return Stream.of(
@@ -102,7 +106,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(12));
+        assertThat(logic.getRules().size(), is(13));
 
 
     }
