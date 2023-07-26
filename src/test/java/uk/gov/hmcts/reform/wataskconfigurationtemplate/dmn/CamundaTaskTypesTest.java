d06355a24a28127471b581cf5a4885e7450bb65f
abhiahirv1 <abhi.ahir@hmcts.net>
2023-07-26 14:48:45 +0100

    Updating DMN and Unit test for WA - Legal Officer Task - Review new case and provide directions - Legal Officer

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
@@ -100,6 +100,10 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
             Map.of(
                 "taskTypeId", "referCase",
                 "taskTypeName", "Refer Case"
+            ),
+            Map.of(
+                "taskTypeId", "reviewNewCaseAndProvideDirectionsLO",
+                "taskTypeName", "Review new case and provide directions - Legal Officer"
             )
         );
         return Stream.of(
@@ -126,7 +130,7 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(1));
         assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(18));
+        assertThat(logic.getRules().size(), is(19));
 
 
     }
