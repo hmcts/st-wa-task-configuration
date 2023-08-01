721fd5c575872a2d6eb2946082e36f8b921251db
abhiahirv1 <abhi.ahir@hmcts.net>
2023-08-01 16:12:47 +0100

    ST-1306- WA - Judicial Task - Review Postponement request - Judge DMN And Unit test

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskCompletionTest.java
@@ -208,6 +208,10 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskType", "reviewListCaseWithin5DaysJudge",
                         "completionMode", "Auto"
+                    ),
+                    Map.of(
+                        "taskType", "reviewPostponementRequestJudge",
+                        "completionMode", "Auto"
                     )
                 )
             )
@@ -228,6 +232,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(32));
+        assertThat(logic.getRules().size(), is(33));
     }
 }
