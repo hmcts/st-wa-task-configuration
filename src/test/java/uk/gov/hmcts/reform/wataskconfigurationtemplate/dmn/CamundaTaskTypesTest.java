50346eb2ede7263fa8f870193c28afc556e1a86a
Rajaram <70951210+rajaram-nagarajan@users.noreply.github.com>
2024-03-26 17:23:25 +0000

    DTSSTCI-674: Updated tests for task types DMN (#103)

--- src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationtemplate/dmn/CamundaTaskTypesTest.java
@@ -19,8 +19,59 @@ import java.util.stream.Stream;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.hmcts.reform.wataskconfigurationtemplate.DmnDecisionTable.WA_TASK_TYPES_ST_CIC_CRIMINALINJURIESCOMPENSATION;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.COMPLETE_HEARING_OUTCOME_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.FOLLOW_UP_NONCOMPLIANCE_OF_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.ISSUE_DECISION_NOTICE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_CASE_WITHDRAWAL_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_CORRECTIONS_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_DIR_RELISTED_CASE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_DIR_RETURNED_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_FURTHER_EVIDENCE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_LISTING_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_WRITTEN_REASONS_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_TIME_EXT_DIR_RETURNED_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_STRIKE_OUT_DIR_RETURNED_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_STAY_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_SET_ASIDE_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_RULE27_DECISION_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_REINSTATEMENT_DECISION_NOTICE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_POSTPONEMENT_DIR_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_OTHER_DIR_RETURNED_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.PROCESS_DIR_RELISTED_CASE_WITHIN_5DAYS_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REFER_CASE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REGISTER_NEW_CASE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_NEW_CASE_PROVIDE_DIR_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_CORRECTIONS_REQ_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LISTING_DIR_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LIST_CASE_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LIST_CASE_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LIST_CASE_WITHIN_5DAYS_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LIST_CASE_WITHIN_5DAYS_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_LISTING_DIR_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_NEW_CASE_PROVIDE_DIR_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_OTHER_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_OTHER_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_POSTPONEMENT_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_POSTPONEMENT_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_REINSTATEMENT_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_REINSTATEMENT_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_RULE27_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_RULE27_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_SET_ASIDE_REQ_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_STAY_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_STAY_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_STRIKE_OUT_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_STRIKE_OUT_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_TIME_EXT_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_TIME_EXT_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_WITHDRAWAL_REQ_JUDGE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_WITHDRAWAL_REQ_LO_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.REVIEW_WRITTEN_REASONS_REQ_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.STITCH_COLLATE_HEARING_BUNDLE_TASK;
+import static uk.gov.hmcts.reform.wataskconfigurationtemplate.utils.CamundaTaskConstants.VET_NEW_CASE_DOCUMENTS_TASK;
 
 class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
+
     @BeforeAll
     public static void initialization() {
         CURRENT_DMN_DECISION_TABLE = WA_TASK_TYPES_ST_CIC_CRIMINALINJURIESCOMPENSATION;
@@ -29,192 +80,204 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
     static Stream<Arguments> scenarioProvider() {
         List<Map<String, String>> taskTypes = List.of(
             Map.of(
-                "taskTypeId", "processCaseWithdrawalDirections",
+                "taskTypeId", PROCESS_CASE_WITHDRAWAL_DIR_TASK,
                 "taskTypeName", "Process Case Withdrawal Directions"
             ),
             Map.of(
-                "taskTypeId", "processRule27Decision",
+                "taskTypeId", PROCESS_RULE27_DECISION_TASK,
                 "taskTypeName", "Process Rule 27 decision"
             ),
             Map.of(
-                "taskTypeId", "processListingDirections",
+                "taskTypeId", PROCESS_LISTING_DIR_TASK,
                 "taskTypeName", "Process listing directions"
             ),
             Map.of(
-                "taskTypeId", "processDirectionsReListedCase",
+                "taskTypeId", PROCESS_DIR_RELISTED_CASE_TASK,
                 "taskTypeName", "Process directions re. listed case"
             ),
             Map.of(
-                "taskTypeId", "processDirectionsReListedCaseWithin5Days",
+                "taskTypeId", PROCESS_DIR_RELISTED_CASE_WITHIN_5DAYS_TASK,
                 "taskTypeName", "Process directions re. listed case (within 5 days)"
             ),
             Map.of(
-                "taskTypeId", "processSetAsideDirections",
+                "taskTypeId", PROCESS_SET_ASIDE_DIR_TASK,
                 "taskTypeName", "Process Set Aside directions"
             ),
             Map.of(
-                "taskTypeId", "processCorrections",
+                "taskTypeId", PROCESS_CORRECTIONS_TASK,
                 "taskTypeName", "Process corrections"
             ),
             Map.of(
-                "taskTypeId", "processDirectionsReturned",
+                "taskTypeId", PROCESS_DIR_RETURNED_TASK,
                 "taskTypeName", "Process directions returned"
             ),
             Map.of(
-                "taskTypeId", "processPostponementDirections",
+                "taskTypeId", PROCESS_POSTPONEMENT_DIR_TASK,
                 "taskTypeName", "Process postponement directions"
             ),
             Map.of(
-                "taskTypeId", "processTimeExtensionDirectionsReturned",
+                "taskTypeId", PROCESS_TIME_EXT_DIR_RETURNED_TASK,
                 "taskTypeName", "Process time extension directions returned"
             ),
             Map.of(
-                "taskTypeId", "processReinstatementDecisionNotice",
+                "taskTypeId", PROCESS_REINSTATEMENT_DECISION_NOTICE_TASK,
                 "taskTypeName", "Process Reinstatement decision notice"
             ),
             Map.of(
-                "taskTypeId", "processOtherDirectionsReturned",
+                "taskTypeId", PROCESS_OTHER_DIR_RETURNED_TASK,
                 "taskTypeName", "Process other directions reurned"
             ),
             Map.of(
-                "taskTypeId", "processWrittenReasons",
+                "taskTypeId", PROCESS_WRITTEN_REASONS_TASK,
                 "taskTypeName", "Process Written Reasons"
             ),
             Map.of(
-                "taskTypeId", "processStrikeOutDirectionsReturned",
+                "taskTypeId", PROCESS_STRIKE_OUT_DIR_RETURNED_TASK,
                 "taskTypeName", "Process strike out directions returned"
             ),
             Map.of(
-                "taskTypeId", "processStayDirections",
+                "taskTypeId", PROCESS_STAY_DIR_TASK,
                 "taskTypeName", "Process stay directions"
             ),
             Map.of(
-                "taskTypeId", "issueDecisionNotice",
+                "taskTypeId", ISSUE_DECISION_NOTICE_TASK,
                 "taskTypeName", "Issue Decision Notice"
             ),
             Map.of(
-                "taskTypeId", "completeHearingOutcome",
+                "taskTypeId", COMPLETE_HEARING_OUTCOME_TASK,
                 "taskTypeName", "Complete Hearing Outcome"
             ),
             Map.of(
-                "taskTypeId", "referCase",
+                "taskTypeId", REFER_CASE_TASK,
                 "taskTypeName", "Refer Case"
             ),
             Map.of(
-                "taskTypeId", "vetNewCaseDocuments",
+                "taskTypeId", VET_NEW_CASE_DOCUMENTS_TASK,
                 "taskTypeName", "Vet New Case Documents"
             ),
             Map.of(
-                "taskTypeId", "reviewNewCaseAndProvideDirectionsLO",
+                "taskTypeId", REVIEW_NEW_CASE_PROVIDE_DIR_LO_TASK,
                 "taskTypeName", "Review new case and provide directions - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewTimeExtensionRequestLO",
+                "taskTypeId", REVIEW_TIME_EXT_REQ_LO_TASK,
                 "taskTypeName", "Review Time extension request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewStrikeOutRequestLO",
+                "taskTypeId", REVIEW_STRIKE_OUT_REQ_LO_TASK,
                 "taskTypeName", "Review Strike out request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewStayRequestLO",
+                "taskTypeId", REVIEW_STAY_REQ_LO_TASK,
                 "taskTypeName", "Review stay request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewListingDirectionsLO",
+                "taskTypeId", REVIEW_LISTING_DIR_LO_TASK,
                 "taskTypeName", "Review listing directions - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewWithdrawalRequestLO",
+                "taskTypeId", REVIEW_WITHDRAWAL_REQ_LO_TASK,
                 "taskTypeName", "Review withdrawal request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewRule27RequestLO",
+                "taskTypeId", REVIEW_RULE27_REQ_LO_TASK,
                 "taskTypeName", "Review Rule 27 request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewListCaseLO",
+                "taskTypeId", REVIEW_LIST_CASE_LO_TASK,
                 "taskTypeName", "Review List Case - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewOtherRequestLO",
+                "taskTypeId", REVIEW_OTHER_REQ_LO_TASK,
                 "taskTypeName", "Review Reinstatement request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewListCaseWithin5DaysLO",
+                "taskTypeId", REVIEW_LIST_CASE_WITHIN_5DAYS_LO_TASK,
                 "taskTypeName", "Review list case (within 5 days) - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewPostponementRequestLO",
+                "taskTypeId", REVIEW_POSTPONEMENT_REQ_LO_TASK,
                 "taskTypeName", "Review Postponement request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewReinstatementRequestLO",
+                "taskTypeId", REVIEW_REINSTATEMENT_REQ_LO_TASK,
                 "taskTypeName", "Review Reinstatement request - Legal Officer"
             ),
             Map.of(
-                "taskTypeId", "reviewListCaseWithin5DaysJudge",
+                "taskTypeId", REVIEW_LIST_CASE_WITHIN_5DAYS_JUDGE_TASK,
                 "taskTypeName", "Review list case (within 5 days) - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewPostponementRequestJudge",
+                "taskTypeId", REVIEW_POSTPONEMENT_REQ_JUDGE_TASK,
                 "taskTypeName", "Review Postponement request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewCorrectionsRequest",
+                "taskTypeId", REVIEW_CORRECTIONS_REQ_TASK,
                 "taskTypeName", "Review Corrections request"
             ),
             Map.of(
-                "taskTypeId", "reviewWrittenReasonsRequest",
+                "taskTypeId", REVIEW_WRITTEN_REASONS_REQ_TASK,
                 "taskTypeName", "Review Written Reasons request"
             ),
             Map.of(
-                "taskTypeId", "reviewReinstatementRequestJudge",
+                "taskTypeId", REVIEW_REINSTATEMENT_REQ_JUDGE_TASK,
                 "taskTypeName", "Review Reinstatement request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewSetAsideRequest",
+                "taskTypeId", REVIEW_SET_ASIDE_REQ_TASK,
                 "taskTypeName", "Review Set Aside request"
             ),
             Map.of(
-                "taskTypeId", "reviewStayRequestJudge",
+                "taskTypeId", REVIEW_STAY_REQ_JUDGE_TASK,
                 "taskTypeName", "Review stay request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewNewCaseAndProvideDirectionsJudge",
+                "taskTypeId", REVIEW_NEW_CASE_PROVIDE_DIR_JUDGE_TASK,
                 "taskTypeName", "Review new case and provide directions - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewOtherRequestJudge",
+                "taskTypeId", REVIEW_OTHER_REQ_JUDGE_TASK,
                 "taskTypeName", "Review other request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewWithdrawalRequestJudge",
+                "taskTypeId", REVIEW_WITHDRAWAL_REQ_JUDGE_TASK,
                 "taskTypeName", "Review withdrawal request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewRule27RequestJudge",
+                "taskTypeId", REVIEW_RULE27_REQ_JUDGE_TASK,
                 "taskTypeName", "Review Rule 27 request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewListingDirectionsJudge",
+                "taskTypeId", REVIEW_LISTING_DIR_JUDGE_TASK,
                 "taskTypeName", "Review listing directions - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewListCaseJudge",
+                "taskTypeId", REVIEW_LIST_CASE_JUDGE_TASK,
                 "taskTypeName", "Review List Case - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewStrikeOutRequestJudge",
+                "taskTypeId", REVIEW_STRIKE_OUT_REQ_JUDGE_TASK,
                 "taskTypeName", "Review Strike out request - Judge"
             ),
             Map.of(
-                "taskTypeId", "reviewTimeExtensionRequestJudge",
+                "taskTypeId", REVIEW_TIME_EXT_REQ_JUDGE_TASK,
                 "taskTypeName", "Review Time extention request - Judge"
             ),
             Map.of(
-                "taskTypeId", "followUpNoncomplianceOfDirections",
+                "taskTypeId", FOLLOW_UP_NONCOMPLIANCE_OF_DIR_TASK,
                 "taskTypeName", "Follow up noncompliance of directions"
+            ),
+            Map.of(
+                "taskTypeId", REGISTER_NEW_CASE_TASK,
+                "taskTypeName", "Register New Case"
+            ),
+            Map.of(
+                "taskTypeId", PROCESS_FURTHER_EVIDENCE_TASK,
+                "taskTypeName", "Process Further Evidence"
+            ),
+            Map.of(
+                "taskTypeId", STITCH_COLLATE_HEARING_BUNDLE_TASK,
+                "taskTypeName", "Stitch/collate hearing bundle"
             )
         );
         return Stream.of(
@@ -222,6 +285,15 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
                 taskTypes
             )
         );
+    }
+
+    @Test
+    void check_dmn_changed() {
+        //The purpose of this test is to prevent adding new rows without being tested
+        DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
+        assertThat(logic.getInputs().size(), is(1));
+        assertThat(logic.getOutputs().size(), is(2));
+        assertThat(logic.getRules().size(), is(50));
     }
 
     @ParameterizedTest(name = "retrieve all task type data")
@@ -233,12 +305,4 @@ class CamundaTaskTypesTest extends DmnDecisionTableBaseUnitTest {
         MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(expectedTaskTypes));
     }
 
-    @Test
-    void check_dmn_changed() {
-        //The purpose of this test is to prevent adding new rows without being tested
-        DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getInputs().size(), is(1));
-        assertThat(logic.getOutputs().size(), is(2));
-        assertThat(logic.getRules().size(), is(47));
-    }
 }
