359537a59f151f1e95b5e1a6dced7b5e3a6bab5f
gmmagruder <gabrielle.magruder@gmail.com>
2024-06-20 12:07:52 +0100

    change st to sptribs in camunda url

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -10,7 +10,7 @@ echo 'export ENVIRONMENT=preview'
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-st-task-configuration-pr-${pr}.preview.platform.hmcts.net"
-echo "export CAMUNDA_BASE_URL=https://camunda-st-task-configuration-pr-${pr}.preview.platform.hmcts.net"
+echo "export CAMUNDA_BASE_URL=https://camunda-sptribs-task-configuration-pr-${pr}.preview.platform.hmcts.net"
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://st-task-configuration-pr-${pr}-java"
