9b59affb1a74e1695476bec6c0f6d3a73f6d84d5
gmmagruder <gabrielle.magruder@gmail.com>
2024-06-20 08:51:17 +0100

    change sptribs to sptribs_case_api

--- bin/import-dmn-diagram.sh
+++ bin/import-dmn-diagram.sh
@@ -8,7 +8,7 @@ product=${4}
 
 s2sSecret=${S2S_SECRET:-AABBCCDDEEFFGGHH}
 
-serviceToken=$($(realpath $workspace)/bin/utils/idam-lease-service-token.sh sptribs \
+serviceToken=$($(realpath $workspace)/bin/utils/idam-lease-service-token.sh sptribs_case_api \
   $(docker run --rm toolbelt/oathtool --totp -b ${s2sSecret}))
 
 dmnFilepath="$(realpath $workspace)/src/main/resources"
